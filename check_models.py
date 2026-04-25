c393575acd15ecbd1d3bcbb2b2f2897cfcbd5204
Oluwaseun Olajide <oluwaseunolajide04@gmail.com>
2026-04-25 09:40:58 +0100

    chore: cleanup unnecessary test files and local data logs

--- check_models.py
+++
@@ -1,7 +0,0 @@
-from groq import Groq
-import os
-os.environ["GROQ_API_KEY"] = "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"
-client = Groq(api_key=os.environ["GROQ_API_KEY"])
-models = client.models.list()
-for m in sorted(models.data, key=lambda x: x.id):
-    print(m.id)
